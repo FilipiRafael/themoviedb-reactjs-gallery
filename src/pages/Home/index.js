4c2226a5f42d054e92a0399647c37192fcf4e77e
Filipi Rafael <filipirafael.123@gmail.com>
2022-07-06 19:49:10 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <>
+        <h1>Movies</h1>
+        <ul>
+            <li>
+                <img src="https://upload.wikimedia.org/wikipedia/pt/7/78/Spider-Man_jogo_2018_capa.png" alt="Spider man cover" />
+                <span>Spider man</span>
+            </li>
+            <li>
+                <img src="https://upload.wikimedia.org/wikipedia/pt/7/78/Spider-Man_jogo_2018_capa.png" alt="Spider man cover" />
+                <span>Spider man</span>
+            </li>
+            <li>
+                 <img src="https://upload.wikimedia.org/wikipedia/pt/7/78/Spider-Man_jogo_2018_capa.png" alt="Spider man cover" />
+                <span>Spider man</span>
+            </li>
+        </ul>
+    </>
+  );
+}
+
+export default Home;
